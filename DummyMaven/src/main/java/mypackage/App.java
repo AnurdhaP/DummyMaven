826ccdc2d56d3240e55010934e76e0cdbb654ee4
Anuradha <anuradhap0109@gmail.com>
2023-07-27 14:26:41 +0530

    Second Commit to Maven project

--- DummyMaven/src/main/java/mypackage/App.java
+++ DummyMaven/src/main/java/mypackage/App.java
@@ -5,6 +5,7 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Maven Git Demo");
+		System.out.println("Another line is added");
 
 	}
 
