372304a52a5073bc8e3d8efac5caf2c2b3e6b0b1
Anuradha <anuradhap0109@gmail.com>
2023-07-27 14:29:30 +0530

    Fourth commit

---
+++ DummyMaven/src/main/java/mypackage/AnotherApp.java
@@ -0,0 +1,11 @@
+package mypackage;
+
+public class AnotherApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is another Java file");
+
+	}
+
+}
